c7b07d87a035745053df31fb50888b93068a2ada
The-click <ramzanmal04@mail.ru>
2025-05-09 02:09:11 +0300

    feat: add class and parital mock tests

--- src/05-partial-mocking/index.test.ts
+++ src/05-partial-mocking/index.test.ts
@@ -1,8 +1,18 @@
 // Uncomment the code below and write your tests
-// import { mockOne, mockTwo, mockThree, unmockedFunction } from './index';
+import { mockOne, mockTwo, mockThree, unmockedFunction } from './index';
 
 jest.mock('./index', () => {
-  // const originalModule = jest.requireActual<typeof import('./index')>('./index');
+  const originalModule =
+    jest.requireActual<typeof import('./index')>('./index');
+
+  //Mock the default export and named export 'foo'
+  return {
+    __esModule: true,
+    ...originalModule,
+    mockOne: jest.fn(() => 'mockOne'),
+    mockTwo: jest.fn(() => 'mockTwo'),
+    mockThree: jest.fn(() => 'mockThree'),
+  };
 });
 
 describe('partial mocking', () => {
@@ -11,10 +21,16 @@ describe('partial mocking', () => {
   });
 
   test('mockOne, mockTwo, mockThree should not log into console', () => {
-    // Write your test here
+    const consoleSpy = jest.spyOn(console, 'log');
+    mockOne();
+    mockTwo();
+    mockThree();
+    expect(consoleSpy).not.toHaveBeenCalled();
   });
 
   test('unmockedFunction should log into console', () => {
-    // Write your test here
+    const consoleSpy = jest.spyOn(console, 'log');
+    unmockedFunction();
+    expect(consoleSpy).toHaveBeenCalled();
   });
 });
