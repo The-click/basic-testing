9d62d0f658656d7f159b4aba5375139c3828a1e8
The-click <ramzanmal04@mail.ru>
2025-11-14 16:52:43 +0300

    feat: delete import in 7 test

--- src/07-mocking-lib-api/index.test.ts
+++ src/07-mocking-lib-api/index.test.ts
@@ -1,10 +1,10 @@
 // Uncomment the code below and write your tests
-/* import axios from 'axios';
-import { throttledGetDataFromApi } from './index'; */
 
 describe('throttledGetDataFromApi', () => {
   test('should create instance with provided base url', async () => {
-    // Write your test here
+    jest.useFakeTimers();
+    jest.mock('lodash');
+    jest.mock('axios');
   });
 
   test('should perform request to correct provided url', async () => {
